8ef52b4c7392bb34652cd8ca2669428acef77c0c
413855023 溫永泉 <413855023@o365.tku.edu.tw>
2026-06-07 22:33:04 +0800

    Add contributor profile images

--- contributors.php
+++ contributors.php
@@ -3,37 +3,37 @@
 echo "<h1>Contributors</h1>";
 
 echo "<hr>";
-
+echo "<img src='images/413855155.jpeg' width='200'><br>";
 echo "<h2>張祿品</h2>";
 echo "<p>Student ID: 413855155</p>";
 echo "<p>I am a second-year Computer Science student who is passionate about technology and problem-solving.</p>";
 
 echo "<hr>";
-
+echo "<img src='images/413855023.jpeg' width='200'><br>";
 echo "<h2>溫永泉</h2>";
 echo "<p>Student ID: 413855023</p>";
 echo "<p>I am interested in computer science and programming and enjoy learning new technologies.</p>";
 
 echo "<hr>";
-
+echo "<img src='images/413856146.jpeg' width='200'><br>";
 echo "<h2>潘宏成</h2>";
 echo "<p>Student ID: 413856146</p>";
 echo "<p>I am a first-year student interested in programming and data science.</p>";
 
 echo "<hr>";
-
+echo "<img src='images/413855122.jpeg' width='200'><br>";
 echo "<h2>王文光</h2>";
 echo "<p>Student ID: 413855122</p>";
 echo "<p>I love fishing and I love sleeping.</p>";
 
 echo "<hr>";
-
+echo "<img src='images/413854638.jpeg' width='200'><br>";
 echo "<h2>謝宜成</h2>";
 echo "<p>Student ID: 413854638</p>";
 echo "<p>I am interested in computer science and programming and enjoy learning new technologies.</p>";
 
 echo "<hr>";
-
+echo "<img src='images/414857010.jpeg' width='200'><br>";
 echo "<h2>邱培榮</h2>";
 echo "<p>Student ID: 414857010</p>";
 echo "<p>I am interested in technology and programming and enjoy learning new skills.</p>";
